25c1dd2978e7bd48365f30c4253eeea813f7309d
JerryLX <724733136@qq.com>
2020-02-24 17:47:50 +0800

    add startworker

--- stream.go
+++ stream.go
@@ -2,7 +2,7 @@ package iface
 
 import (
 	"context"
-
+    peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/SJTU-OpenNetwork/go-stream"
 )
 
@@ -10,4 +10,5 @@ type StreamAPI interface {
 	StartStream(context.Context, *stream.Stream) error
 	AddStreamBlock(context.Context, *stream.StreamBlock) error
 	SubscribeStream(context.Context, *stream.StreamConfig) error
+    StartWorker(context.Context, *stream.StreamConfig, peer.ID) error
 }
